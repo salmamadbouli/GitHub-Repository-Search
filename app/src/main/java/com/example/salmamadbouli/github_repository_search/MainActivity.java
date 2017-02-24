82af954c8a03d1c72227f07006dbf3611305a566
Salma Madbouli <salma.moh.m@outlook.com>
2017-02-24 19:40:25 +0200

    Creating the main layout.

--- app/src/main/java/com/example/salmamadbouli/github_repository_search/MainActivity.java
+++ app/src/main/java/com/example/salmamadbouli/github_repository_search/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.salmamadbouli.github_repository_search;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText mSearchBoxEditText;
+    TextView mUrlDisplayTextView;
+    TextView mSearchResultsTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+
     }
 }
